551e558ba607ef5e5130f2500fd4a6b88640aa25
Eamonn O'Brien-Strain <eob@well.com>
2021-06-13 19:08:20 -0700

    Use tf-idf

--- app.js
+++ app.js
@@ -1,11 +1,76 @@
-/* global WordFreq, WordCloud, DOMParser, fetch, articleElement */
+/* global  WordCloud, DOMParser, fetch, articleElement */
 
 const utlPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
 
 const deTag = html => new DOMParser().parseFromString(html, 'text/html').body.textContent || ''
 
-// Create an options object for initialization
-const wordfreqOptions = { workerUrl: 'lib/wordfreq.worker.js' }
+class Terms {
+  constructor () {
+    this.terms = {}
+    this.count = 0
+  }
+
+  addTerm (term) {
+    if (this.terms[term]) {
+      this.terms[term].count++
+    } else {
+      this.terms[term] = { count: 1 }
+    }
+    this.count++
+  }
+}
+
+class Corpus extends Terms {
+  constructor () {
+    super()
+    this.docs = []
+  }
+
+  addDoc (doc) {
+    this.docs.push(doc)
+  }
+
+  inverseDocFrequency (term) {
+    let docsWithTerm = 0
+    for (const doc of this.docs) {
+      if (doc.terms[term]) {
+        ++docsWithTerm
+      }
+    }
+    return Math.log(1.0 * this.docs.length / docsWithTerm)
+  }
+}
+
+class Doc extends Terms {
+  constructor (corpus) {
+    super()
+    this.corpus = corpus
+    this.terms = {}
+    corpus.addDoc(this)
+  }
+
+  addText (text) {
+    for (const term of text.split(/[\W0-9_]+/)) {
+      this.corpus.addTerm(term)
+      this.addTerm(term)
+    }
+  }
+
+  termFrequency (term) {
+    return 1.0 * this.terms[term].count / this.count
+  }
+
+  tfIdf (term) {
+    return this.termFrequency(term) * this.corpus.inverseDocFrequency(term)
+  }
+
+  list () {
+    const unscaled = Object.keys(this.terms).map(term => [term, this.tfIdf(term)]).filter(([_, value]) => value > 0)
+    return unscaled.map(([term, value]) => [term, Math.round(value * 1000)])
+  }
+}
+
+const corpus = new Corpus()
 
 ;(async () => {
   const response = await fetch('https://api.joinmastodon.org/servers')
@@ -16,23 +81,23 @@ const wordfreqOptions = { workerUrl: 'lib/wordfreq.worker.js' }
   const biggestSize = communities[0].last_week_users
 
   for (const community of communities) {
-    const sizeWidth = 100.0 * community.last_week_users / biggestSize
-    const sectionElment = document.createElement('section')
-    // console.log(community)
-    articleElement.insertAdjacentElement('beforeend', sectionElment)
-    sectionElment.insertAdjacentHTML('beforeend',
-        `
-        <h2>${community.domain}</h2>
-        <p style="width:${sizeWidth}vw;height:2em;background:red">
-        ${community.last_week_users} users last week
-        </p>
-        `
-    )
     try {
       const apiResponse = await fetch(`https://${community.domain}/api/v1/timelines/public?limit=100`)
       const timeline = await apiResponse.json()
+      const sizeWidth = 100.0 * community.last_week_users / biggestSize
+      const sectionElment = document.createElement('section')
+      // console.log(community)
+      articleElement.insertAdjacentElement('beforeend', sectionElment)
+      sectionElment.insertAdjacentHTML('beforeend',
+          `
+          <h2>${community.domain}</h2>
+          <p style="width:${sizeWidth}vw;height:2em;background:red">
+          ${community.last_week_users} users last week
+          </p>
+          `
+      )
       // console.log(timeline)
-      const wordfreq = WordFreq(wordfreqOptions)
+      const doc = new Doc(corpus)
       for (const toot of timeline) {
         sectionElment.insertAdjacentHTML('beforeend', `
               <img src="${toot.account.avatar}">
@@ -40,32 +105,30 @@ const wordfreqOptions = { workerUrl: 'lib/wordfreq.worker.js' }
         )
         const text = deTag(toot.content).split(utlPattern).join(' ')
         // console.log(text)
-        wordfreq.process(text)
+        doc.addText(text)
       }
-      wordfreq.getList(list => {
-        // console.log(list)
-        const canvasElement = document.createElement('canvas')
-        canvasElement.setAttribute('width', window.innerWidth)
-        canvasElement.setAttribute('height', window.innerWidth / 2)
-        const canvasWidth = canvasElement.width
-        sectionElment.insertAdjacentElement('beforeend', canvasElement)
-        WordCloud(canvasElement, {
-          list,
-          shrinkToFit: true,
-          gridSize: Math.round(16 * canvasWidth / 1024),
-          weightFactor: size => Math.pow(size, 2.3) * canvasWidth / 1024,
-          fontFamily: 'Times, serif',
-          color: (word, weight) => (weight === list[0][1]) ? '#f02222' : '#c09292',
-          rotateRatio: 0.5,
-          rotationSteps: 2,
-          backgroundColor: '#ffe0e0'
-        })
+      // console.log(doc.list())
+      // wordfreq.getList(list => {
+      // console.log(list)
+      const canvasElement = document.createElement('canvas')
+      canvasElement.setAttribute('width', window.innerWidth)
+      canvasElement.setAttribute('height', window.innerWidth / 2)
+      const canvasWidth = canvasElement.width
+      sectionElment.insertAdjacentElement('beforeend', canvasElement)
+      WordCloud(canvasElement, {
+        list: doc.list(),
+        shrinkToFit: true,
+        gridSize: Math.round(16 * canvasWidth / 1024),
+        weightFactor: size => Math.pow(size, 2.3) * canvasWidth / 1024,
+        fontFamily: 'Times, serif',
+        color: (word, weight) => '#c09292', // (weight === list[0][1]) ? '#f02222' : '#c09292',
+        rotateRatio: 0.5,
+        rotationSteps: 2,
+        backgroundColor: '#ffe0e0'
       })
+      // })
     } catch (e) {
-      sectionElment.insertAdjacentHTML('beforeend', `
-              <p>${e}</p>
-            `
-      )
+      console.warning('Ignoring', community.domain, e)
     }
   }
 })()
