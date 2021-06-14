a881a54a911b7b93f1f3e1c9912393c80bfe31a0
Eamonn O'Brien-Strain <eob@well.com>
2021-06-13 21:11:53 -0700

    Run fetches in parallel

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* global  DOMParser, fetch, articleElement */
+/* global DOMParser, fetch, articleElement */
 
 const utlPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
 
@@ -76,69 +76,63 @@ class Doc extends Terms {
 
 const corpus = new Corpus()
 
-;(async () => {
-  const response = await fetch('https://api.joinmastodon.org/servers')
-  const communities = await response.json()
-  // console.log(communities)
-  communities.sort((a, b) => b.last_week_users - a.last_week_users)
+fetch('https://api.joinmastodon.org/servers')
+  .then(response => response.json())
+  .then(communities => {
+    communities.sort((a, b) => b.last_week_users - a.last_week_users)
 
-  const biggestSize = communities[0].last_week_users
+    const biggestSize = communities[0].last_week_users
 
-  const progressElement = document.createElement('progress')
-  progressElement.max = communities.length
-  articleElement.insertAdjacentElement('beforeend', progressElement)
+    const progressElement = document.createElement('progress')
+    progressElement.max = communities.length
+    articleElement.insertAdjacentElement('beforeend', progressElement)
 
-  const later = []
+    const later = []
 
-  let count = 0
-  for (const community of communities) {
-    progressElement.value = ++count
-    try {
-      const apiResponse = await fetch(`https://${community.domain}/api/v1/timelines/public?local=true&limit=100`)
-      const timeline = await apiResponse.json()
-      if (timeline.length === 0) {
-        console.warn('No toots from', community.domain)
-        continue
-      }
-      const sizeWidth = 100.0 * community.last_week_users / biggestSize
-      const sectionElment = document.createElement('section')
-      // console.log(community)
-      articleElement.insertAdjacentElement('beforeend', sectionElment)
-      sectionElment.insertAdjacentHTML('beforeend',
+    let count = 0
+    const promises = []
+    for (const community of communities) {
+      promises.push(fetch(`https://${community.domain}/api/v1/timelines/public?local=true&limit=100`)
+        .then(apiResponse => apiResponse.json())
+        .then(timeline => {
+          progressElement.value = ++count
+          if (timeline.length === 0) {
+            console.warn('No toots from', community.domain)
+            return
+          }
+          const sizeWidth = 100.0 * community.last_week_users / biggestSize
+          const sectionElment = document.createElement('section')
+          articleElement.insertAdjacentElement('beforeend', sectionElment)
+          sectionElment.insertAdjacentHTML('beforeend',
           `
           <h2>${community.domain}</h2>
           <div class="bar" style="width:${sizeWidth}vw"></div>
           <p>${community.last_week_users} users last week</p>
           `
-      )
-      // console.log(timeline)
-      const doc = new Doc(corpus)
-      // console.log(doc.list())
-      // const canvasElement = document.createElement('canvas')
-      // canvasElement.setAttribute('width', window.innerWidth / 2)
-      // canvasElement.setAttribute('height', window.innerWidth / 4)
-      // const canvasWidth = canvasElement.width
-      // sectionElment.insertAdjacentElement('beforeend', canvasElement)
-      for (const toot of timeline) {
-        sectionElment.insertAdjacentHTML('beforeend', `
+          )
+          const doc = new Doc(corpus)
+          for (const toot of timeline) {
+            sectionElment.insertAdjacentHTML('beforeend', `
               <img src="${toot.account.avatar}">
             `
-        )
-        const text = deTag(toot.content).split(utlPattern).join(' ')
-        // console.log(text)
-        doc.addText(text)
-      }
-      later.push({ doc, sectionElment })
-    } catch (e) {
-      console.warn('Ignoring', community.domain, e)
+            )
+            const text = deTag(toot.content).split(utlPattern).join(' ')
+            doc.addText(text)
+          }
+          later.push({ doc, sectionElment })
+        }).catch(e => {
+          progressElement.value = ++count
+          console.warn('Ignoring', community.domain, e)
+        }))
     }
-  }
-  for (const { doc, sectionElment } of later) {
-    for (const [term, value] of doc.list()) {
-      sectionElment.insertAdjacentHTML('beforeend', `
+    Promise.allSettled(promises).then(() => {
+      for (const { doc, sectionElment } of later) {
+        for (const [term, value] of doc.list()) {
+          sectionElment.insertAdjacentHTML('beforeend', `
             <span style="font-size:${value * 100}px">${term}</span>
           `
-      )
-    }
-  }
-})()
+          )
+        }
+      }
+    })
+  })
